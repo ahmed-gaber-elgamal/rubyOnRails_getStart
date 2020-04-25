e2fc5100331b1bd5bc6af1dc38e58812ac39a8ce
ahmed-gaber-elgamal <yakan44444@gmail.com>
2020-04-25 21:55:36 +0200

    finish sign up &sign in feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
+
   get 'welcome/index'
   resources :articles do
     resources :comments
